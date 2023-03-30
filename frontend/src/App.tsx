db723c2dd40599b8f03abea5a1ee01a788caaa33
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2023-03-30 15:57:57 -0300

    feat: add initial integration of backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 function App() {
   const [count, setCount] = useState(0)
-
+  const [logs,setLogs] = useState([])
+  useEffect(()=>{
+    axios.get("http://10.0.0.115:5005").then(response =>{
+      console.log(response.data)
+      setLogs(response.data)
+    })
+  },[])
   return (
     <div className="App">
+      {logs.map(log => <p key={log._id}>{JSON.stringify(log)}</p>)}
     </div>
   )
 }
