f5ba9c515394908e99183e54cb0cd06637f64acc
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2023-04-11 19:46:11 -0300

    feat: initial integrate with logs

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,18 @@
-
+"use client"
 import ButtonFilter from './components/ButtonFilter'
 import Chart from './components/Chart'
 import styles from './page.module.css'
+import useSWR from 'swr'
+import api from '../../config/api'
 
-
+const fetcher = (url:string) => {
+  console.log("Oio")
+  return api.get(url)
+}
 export default function Home() {
+  const {data} = useSWR("logs",fetcher)
+  console.log(data)
+  console.log(process.env.NEXT_PUBLIC_URL)
   return (
     <main className={styles.main}>
       <div className={styles.container_bar}>
@@ -22,7 +30,35 @@ export default function Home() {
       </div>
 
       </div>
+      <div>
       <p className={styles.title}>Logs</p>
+    <table className={styles.table}>
+      <thead>
+      <tr>
+      <th>Programa</th>
+      <th>Prioridade</th>
+      <th>Mensagem</th>
+      <th>Data</th>
+      <th>Host</th>
+      <th>Processo</th>
+    </tr>
+      </thead>
+      <tbody>
+        {data?.data?.map(log =>(
+          <tr key={log._id}>
+
+      <td>{log.program}</td>
+      <td>{log.priority}</td>
+      <td>{log.message}</td>
+      <td>{log.isodate}</td>
+      <td>{log.host}</td>
+      <td>{log.facility}</td>
+          </tr>
+        ))}
+
+      </tbody>
+    </table>
+      </div>
     </main>
   )
 }
